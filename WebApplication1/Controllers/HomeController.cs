a41a36ec27715f19841f34a1164bd8817f87947b
Patrice PETIT <ppetit@wynd.eu>
2018-03-23 10:03:27 +0100

    test nameof keyword

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace LanguageFeatures.Controllers
                 decimal? price = p?.Price ?? 0;
                 string relatedName = p?.Related?.Name ?? "<None>";
 
-                results.Add($"Name: {name}, Price: {price}, Related: {relatedName}");
+                results.Add($"{nameof(p.Name)}: {name}, {nameof(p.Price)}: {price}, Related: {relatedName}");
                 //results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
             }
 
